fe6cfb6d5167092820260767e0f225dad150aa52
denev6 <dev.seongjin@gmail.com>
2025-01-23 01:50:44 +0900

    post ml-api

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250110.102057';
+const cacheName = 'chirpy-20250122.202415';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
