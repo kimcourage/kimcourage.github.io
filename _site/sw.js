dacb436911c5840b303064e50a93536f1562191e
denev6 <sungjin.code@gmail.com>
2025-03-06 01:35:24 +0900

    post EEG-ADHD

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250301.160407';
+const cacheName = 'chirpy-20250306.013506';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
