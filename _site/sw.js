91989097f654f44049e44298bc53a45da1201e81
Kim Yonggi <yonggi1124@gmail.com>
2025-04-03 11:25:13 +0900

    fix

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250403.104726';
+const cacheName = 'chirpy-20250403.112503';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
